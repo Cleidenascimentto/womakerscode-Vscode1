cbac636d5e408c574f700f3b5abb90dd67930f0f
Cleidenascimento <cleidenascimentto@hotmail.com>
2022-05-05 21:00:31 -0300

    Primeiro commit Aula Vs Code

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+{
+using system;
+
+namespace womakerscode
+{
+    static class  program 
+{
+    static void  main(string[] args)
+    {
+        Metodo();
+Console.WriteLine("Hello, World!" + nome);
+
+ }
+
+ private static object Metodoname()
+ {
+     return "Cleide";
+ }
+
+}
+}
